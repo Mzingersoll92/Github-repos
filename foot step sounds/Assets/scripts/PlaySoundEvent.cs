6f2375896e57d5600652afb9e31c8148078aaaca
Mzingersoll92 <mzingersoll92@gmail.com>
2017-11-16 11:56:21 -0800

    foot step sounds

---
+++ foot step sounds/Assets/scripts/PlaySoundEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(AudioSource))]
+public class PlaySoundEvent : MonoBehaviour
+{
+    public AudioClip mySound;
+    public AudioSource mySource;
+
+    private void Awake()
+    {
+        mySource = GetComponent<AudioSource>();
+    }
+
+    void Step()
+    {
+        mySource.clip = mySound;
+        mySource.Play();
+    }
+}
